74023916f67ef2899308886733711fa66b928ba6
mos-au <m.fereidouni70@yahoo.com>
2024-04-14 15:11:33 +0330

    implement createNew page

--- src/pages/CreateNew/CreateNewPage.js
+++ src/pages/CreateNew/CreateNewPage.js
@@ -1,5 +1,106 @@
-export const CreateNewPage = () => {
-  return <div>Create New</div>;
+import { useState } from "react";
+import styles from "./CreateNewPage.module.css";
+import { useNavigate } from "react-router-dom";
+
+const CreateNewPage = () => {
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isLiked, setIsLiked] = useState(false);
+  const [file, setFile] = useState();
+  const [hasError, setHasError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleCreateNew = async (e) => {
+    e.preventDefault();
+    if (!title || !description || !author) {
+      setHasError(true);
+      return;
+    }
+    setHasError(false);
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const res = await fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData,
+      });
+      const image = await res.json();
+
+      const newPost = {
+        profileName: author,
+        title,
+        description,
+        imageUrl: image.filename,
+        profileImage: "https://avatars.githubusercontent.com/u/51633191",
+        isLiked,
+        likes: Math.floor(Math.random() * 9991),
+      };
+
+      await fetch("http://localhost:5000/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newPost),
+      });
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      {hasError && <div className={styles.error}>All fields are required</div>}
+      <form>
+        <label>
+          Author
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </label>
+        <label>
+          Title
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Description
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+        <div>
+          <label>
+            Image
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+          </label>
+        </div>
+        <label>
+          Is Liked?
+          <input
+            type="checkbox"
+            value={isLiked}
+            onChange={(e) => setIsLiked(e.target.checked)}
+          />
+        </label>
+        <div className={styles["button-wrapper"]}>
+          <button onClick={handleCreateNew}>Create New</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateNewPage;
