bc930808954579687fe3b8258811f50850f0d20c
mos-au <m.fereidouni70@yahoo.com>
2024-04-14 14:59:33 +0330

    add PostPage

--- src/pages/Post/PostPage.js
+++ src/pages/Post/PostPage.js
@@ -1,5 +1,38 @@
-export const PostPage = () => {
-  return <div>Post</div>;
+import React, { useState, useEffect } from "react";
+import Post from "../../components/Post/Post";
+import { useParams } from "react-router-dom";
+
+const PostPage = () => {
+  const [post, setPost] = useState(null);
+  const { postId } = useParams();
+
+  useEffect(() => {
+    getPost();
+  }, [postId]);
+
+  const getPost = async () => {
+    const postIdParam = postId ?? "";
+    const response = await fetch(`http://localhost:5000/posts/${postIdParam}`);
+    const post = await response.json();
+
+    if (!post.imageUrl.startsWith("https")) {
+      const res = await fetch(`http://localhost:5000/files/${post.imageUrl}`);
+      const data = await res.blob();
+      post.image = data;
+    }
+
+    setPost(post);
+  };
+
+  if (!post) {
+    return <div>Loading....</div>;
+  }
+
+  return (
+    <div className="container">
+      <Post post={post} fullPage={true} />
+    </div>
+  );
 };
 
 export default PostPage;
